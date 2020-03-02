580d14716d088bf72a3fb361b2cec1d006c52e54
James Loghry <james.loghry@edlconsulting.com>
2020-03-01 22:03:30 -0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -37,7 +37,7 @@ _git_push() {
 
 # PROGRAM
 echo "Installing prettier..."
-npm install prettier prettier-plugin-apex @prettier/plugin-xml
+npm install --silent --global prettier prettier-plugin-apex @prettier/plugin-xml
 
 echo "JAVAHOME: $JAVA_HOME"
 
